abecb2aa97ef28b22f59c4d50fc91c6a18febcdf
UNKJay <1417752158@qq.com>
2020-03-15 18:04:43 +0800

    finish Chap8 homework

---
+++ Chap8/C8-3.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+class Shape{
+public:
+    Shape() {}
+    virtual ~Shape() {}
+};
+
+class Triangle: public Shape{
+public:
+    Triangle() {}
+    ~Triangle() {}
+};
+
+class Rectangle: public Shape {
+public:
+    Rectangle() {}
+    ~Rectangle() {}
+};
+
+/*用dynamic_cast类型转换操作符完成该函数*/
+int getVertexCount(Shape * b){  
+    if (dynamic_cast<Triangle *>(b))
+        return 3;
+    if (dynamic_cast<Rectangle *>(b))
+        return 4;
+    return 0;
+}
+
+int main() {
+    Shape s;
+    cout << getVertexCount(&s) << endl;
+    Triangle t;
+    cout << getVertexCount(&t) << endl;
+    Rectangle r;
+    cout << getVertexCount(&r) << endl;
+}
